f45d12e3d5beef1a9e5d867564de748350c7e226
Kiara-Bilyk <160333274+Kiara-Bilyk@users.noreply.github.com>
2024-09-10 20:19:19 -0400

    Add files via upload

---
+++ Chapter 1 Projects/project01-02.js
@@ -0,0 +1,16 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Kiara Bilyk
+    Date:   09-10-2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme";
+
+let service4Name = "Ultimate", service1Speed = "50 Mbps";
+let service2Speed = "100 Mbps", service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
